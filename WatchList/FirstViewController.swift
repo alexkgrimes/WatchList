37bec3852495f49bad40ff84706a7ff2f139f9c5
Alex Grimes <alexkgrimes@gmail.com>
2019-07-23 15:01:59 -0400

    Initial Commit

---
+++ WatchList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WatchList
+//
+//  Created by Alex Grimes on 7/23/19.
+//  Copyright © 2019 Alex Grimes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WatchList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WatchList
+//
+//  Created by Alex Grimes on 7/23/19.
+//  Copyright © 2019 Alex Grimes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
